e732a088644ca53ca1428d0fd23696dc76df48f8
wautersN <wauters.niels@gmail.com>
2015-10-23 16:56:20 +0200

    1 rap puschke

--- app/src/main/java/com/example/waute/testgithub/MainActivity.java
+++ app/src/main/java/com/example/waute/testgithub/MainActivity.java
@@ -60,4 +60,5 @@ public class MainActivity extends AppCompatActivity {
     //voor de zoveelste keer een nieuwe branch
     //eeste push op hoogte 1
     //nieuwe Testhoogte2
+    //1 push op hoogte2
 }
