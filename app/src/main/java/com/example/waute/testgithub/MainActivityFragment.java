80403ef7c7d5c625475f0fb588e49dc296794ac8
wautersN <wauters.niels@gmail.com>
2015-10-23 09:30:46 +0200

    2de commit

--- app/src/main/java/com/example/waute/testgithub/MainActivityFragment.java
+++ app/src/main/java/com/example/waute/testgithub/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.example.waute.testgithub;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,5 +18,6 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+        //test
     }
 }
